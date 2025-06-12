4e70809bfefd5919ba44a61e179a70e5d5eada2c
mahipal kachhad <mahipalkac@gmail.com>
2025-06-12 10:14:05 +0530

    still working

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const fs = require("fs");
 
-fs.writeFile("fs.txt", "mahipal kachhad", (err) => {
-  if (err) throw err;
-  else console.log("added");
-});
+// fs.writeFile("fs.txt", "mahipal kachhad", (err) => {
+//   if (err) throw err;
+//   else console.log("added");
+// });
 
+// fs.rm("fs.txt", (err) => {
+//   if (err) throw err;
+//   else console.log("removed");
+// });
+
+fs.readFile("fs.txt","utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
